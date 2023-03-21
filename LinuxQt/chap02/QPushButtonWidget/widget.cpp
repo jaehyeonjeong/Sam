d2f05e4deb6f97c2ec63ba5d214d272bc4d58b57
jaehyeonjeong <definejae234@gmail.com>
2023-03-21 17:01:34 +0900

    QPushButtonWidget

--- LinuxQt/chap02/QPushButtonWidget/widget.cpp
+++ LinuxQt/chap02/QPushButtonWidget/widget.cpp
@@ -1,12 +1,17 @@
 #include <QApplication>
 #include <QPushButton>
+#include <QLabel>
 
 #include "widget.h"
 
 Widget::Widget(QWidget* parent) : QWidget(parent)
 {
 	QPushButton* quit = new QPushButton("Quit", this);
+	quit->move(10, 10);
 	quit->resize(75, 35);
+
+	QLabel *label = new QLabel("<font color=blue>Hello Qt!</font>", this);
+	label->setGeometry(10, 50, 75, 35);
 	connect(quit, SIGNAL(clicked()), qApp, SLOT(quit()));
 }
 
