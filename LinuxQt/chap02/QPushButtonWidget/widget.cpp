ff3367ad0afbe0a4f4ac54a811d25d100c3a0476
jaehyeonjeong <definejae234@gmail.com>
2023-03-21 16:37:34 +0900

    QPushButtonWidget

---
+++ LinuxQt/chap02/QPushButtonWidget/widget.cpp
@@ -0,0 +1,15 @@
+#include <QApplication>
+#include <QPushButton>
+
+#include "widget.h"
+
+Widget::Widget(QWidget* parent) : QWidget(parent)
+{
+	QPushButton* quit = new QPushButton("Quit", this);
+	quit->resize(75, 35);
+	connect(quit, SIGNAL(clicked()), qApp, SLOT(quit()));
+}
+
+Widget::~Widget()
+{
+}
